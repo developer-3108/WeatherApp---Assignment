2cb3874fb89db3393008ff9f697f2e43f16f455b
developer-3108 <developer.akshat310804@gmail.com>
2025-11-18 13:12:16 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Akshat Srivastava on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
